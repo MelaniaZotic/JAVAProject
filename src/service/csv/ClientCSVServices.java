4d2ebe84ff76b599acd6698cfd957ec1de009f6f
MelaniaZotic <82153993+MelaniaZotic@users.noreply.github.com>
2022-05-11 22:07:42 +0300

    2

--- src/service/csv/ClientCSVServices.java
+++ src/service/csv/ClientCSVServices.java
@@ -10,42 +10,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ClientCSVServices implements GenericCSV<Client> {
-   public static final ClientCSVServices INSTANCE = new ClientCSVServices();
+    public static final ClientCSVServices INSTANCE = new ClientCSVServices();
+    private ClientCSVServices(){}
 
-   private ClientCSVServices(){
-
-   }
-   public static ClientCSVServices getInstance(){return INSTANCE;}
-
-
-
+    public static ClientCSVServices getInstance(){return INSTANCE;}
 
 
     @Override
     public void write(Client client){
-       try(FileWriter fileWriter = new FileWriter("files/clients.cvs",true)){
-           fileWriter.write(client.getfirstName()+ "," + client.getlastName() + "," + client.getphoneNo() + "," + client.getUserClient() + "," + client.getAddress() + '\n');
-           fileWriter.flush();
-
-       }catch (IOException e){
-           System.out.println("Error writing to file!");
-       }
-
+        try(FileWriter fileWriter = new FileWriter("files/clients.csv",true)){
+            fileWriter.write(client.getlastName() + ", " + client.getfirstName() + "," + client.getphoneNo() + ", "
+                    + client.getUserClient() + "," + client.getAddress() );
+        }catch (IOException Ex){
+            System.out.println("Error writing to file!");
+        }
     }
 
     @Override
     public List<Client> read(){
         List<Client> clients = new ArrayList<>();
-        try(BufferedReader bufferedReader = new BufferedReader(new FileReader("file/clients.cvs"))) {
+        try(BufferedReader bufferedReader = new BufferedReader(new FileReader("files/clients.csv"))){
             String l;
-            while ((l = bufferedReader.readLine()) != null) {
-                String[] tokens = l.split(",");
-                Client client = new Client(tokens[0], tokens[1], tokens[2], Integer.parseInt(tokens[3]), tokens[4]);
+            while ((l=bufferedReader.readLine())!=null){
+                String[] cl = l.split(",");
+                Client client = new Client(cl[0], cl[1], cl[2], Integer.parseInt(cl[3]), cl[4] );
+
                 clients.add(client);
             }
-        }catch (IOException  | NumberFormatException e){
-            System.out.println("Error reading from file!");
-
+        }catch(IOException ex){
+            System.out.println("Error reading to file");
         }
         return clients;
     }
